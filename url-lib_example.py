324660af3b11f3e8b840933a25d8080258a2eb95
Michael Holloway <mholloway@audiophonic.com>
2019-12-03 22:59:02 -0800

    few more

--- url-lib_example.py
+++ url-lib_example.py
@@ -1,4 +1,4 @@
-# urllib is a library that doe all the socket work for us and
+# urllib is a library that does all the socket work for us and
 # makes web pages look like a file.
 
 # the idea here is that the socket .connect, .send, .recv methods
